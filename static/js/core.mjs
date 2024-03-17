85fee48ece6efb76848196692e04be0cae9f0b3f
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2024-03-17 15:04:12 +0000

    wip - add static folder and begin js

---
+++ static/js/core.mjs
@@ -0,0 +1,20 @@
+/**
+ * Encrypts provided plaintext via the WebCrypto API.
+ * @param {string} plainText The text to be encrypted.
+ * @returns {string} A string consisting of the encrypted secret, salt, and IV.
+ */
+function encrypt(plainText) {
+}
+
+/**
+ * Decrypts encrypted ciphertext with a given password.
+ * @param {string} cipherText Encrypted ciphertext returned from the @see{encrypt} function.
+ * @param {string} password The plaintext password to attempt to decrypt the ciphertext with.
+ */
+function decrypt(cipherText, password) {
+}
+
+export {
+    encrypt,
+    decrypt
+}
