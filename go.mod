04829b5568a268fd164d2fde4c210bbb9d7ae71a
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2024-03-31 21:43:01 +0100

    add env vars for important settings

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.18.0 // indirect
--- shareasecret.go
+++ shareasecret.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/lsymds/shareasecret/internal/shareasecret"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	application, err := shareasecret.NewApplication("file:shareasecret.db", "http://127.0.0.1:8994")
+	// extract any required environment variables
+	err := godotenv.Load()
 	if err != nil {
 		panic(err)
+	}
+
+	dbPath := os.Getenv("SHAREASECRET_DB_PATH")
+	if dbPath == "" {
+		log.Error().Msg("SHAREASECRET_DB_PATH not set")
+		os.Exit(1)
+	}
+
+	baseUrl := os.Getenv("SHAREASECRET_BASE_URL")
+	if baseUrl == "" {
+		log.Error().Msg("SHAREASECRET_BASE_URL not set")
+		os.Exit(1)
+	}
+
+	// initialize the wrapper application
+	application, err := shareasecret.NewApplication("file:"+dbPath, baseUrl)
+	if err != nil {
+		log.Error().Err(err).Msg("initializing application")
+		os.Exit(1)
 	}
 
 	// run any jobs
 	application.RunDeleteExpiredSecretsJob()
 
-	panic(http.ListenAndServe("127.0.0.1:8994", application))
+	// serve all HTTP endpoints
+	log.Info().Msg("starting HTTP server on 127.0.0.1:8994")
+	err = http.ListenAndServe("127.0.0.1:8994", application)
+	if err != nil {
+		log.Error().Err(err).Msg("listen and serve")
+		os.Exit(1)
+	}
 }
