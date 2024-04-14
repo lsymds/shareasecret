ae43c6b586149d23a65ecd58d9f1401165b68cce
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2024-04-14 14:45:19 +0100

    move create secret test utility

--- internal/shareasecret/web_test.go
+++ internal/shareasecret/web_test.go
@@ -236,42 +236,6 @@ type consumedResponse struct {
 	cookies    []*http.Cookie
 }
 
-func createSecret(t *testing.T, deletedAt time.Time, deletionReason string) (string, string) {
-	accessID, _ := secureID(24)
-	managementID, _ := secureID(24)
-
-	var dbDeletedAt sql.NullInt64
-	var dbDeletionReason sql.NullString
-
-	if (deletedAt == time.Time{}) {
-		dbDeletedAt = sql.NullInt64{}
-		dbDeletionReason = sql.NullString{}
-	} else {
-		dbDeletedAt = sql.NullInt64{Valid: true, Int64: deletedAt.UnixMilli()}
-		dbDeletionReason = sql.NullString{Valid: true, String: deletionReason}
-	}
-
-	_, err := app.db.db.Exec(
-		`
-			INSERT INTO secrets (access_id, management_id, maximum_views, ttl, cipher_text, deleted_at, deletion_reason, created_at)
-			VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-		`,
-		accessID,
-		managementID,
-		1,
-		30,
-		"a.b.c",
-		dbDeletedAt,
-		dbDeletionReason,
-		time.Now().UnixMilli(),
-	)
-	if err != nil {
-		t.Errorf("creating secret: %v", err)
-	}
-
-	return accessID, managementID
-}
-
 func responseIsRedirectTo(r consumedResponse, to string) bool {
 	return r.statusCode == 303 && r.headers.Get("Location") == to
 }
