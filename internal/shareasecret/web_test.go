9ecabace4d0deba770e7661a98712b990135a797
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2024-04-14 14:25:48 +0100

    add more tests

--- internal/shareasecret/web_test.go
+++ internal/shareasecret/web_test.go
@@ -63,7 +63,7 @@ func TestSecretManagement(t *testing.T) {
 	t.Run("redirects home if secret has been deleted", func(t *testing.T) {
 		_, managementID := createSecret(t, time.Now(), deletionReasonUserDeleted)
 
-		r := get(t, app.handleManageSecret, fmt.Sprintf("/manage-secret/%v", managementID), func(r *http.Request) { r.SetPathValue("managementID", managementID) })
+		r := get(t, app.handleManageSecret, func(r *http.Request) { r.SetPathValue("managementID", managementID) })
 
 		if r.statusCode != 303 {
 			t.Errorf("expected 303 status code, got %v", r.statusCode)
@@ -96,6 +96,87 @@ func TestSecretManagement(t *testing.T) {
 	})
 }
 
+func TestSecretAccess(t *testing.T) {
+	t.Run("redirects home if secret has been deleted", func(t *testing.T) {
+		accessID, _ := createSecret(t, time.Now(), deletionReasonUserDeleted)
+
+		r := get(t, app.handleAccessSecretInterstitial, func(r *http.Request) { r.SetPathValue("accessID", accessID) })
+
+		if r.statusCode != 303 {
+			t.Errorf("expected 303 status code, got %v", r.statusCode)
+		} else if r.headers.Get("Location") != "/" {
+			t.Errorf("expected redirect to /, got %v", r.headers.Get("Location"))
+		} else if c := r.cookies[0]; c.Name != "flash_err" {
+			t.Errorf("expected flash_err cookie to be present")
+		}
+	})
+
+	t.Run("marks secret as deleted if maximum views is reached", func(t *testing.T) {
+		accessID, _ := createSecret(t, time.Time{}, "")
+
+		r := post(t, app.handleCreateSecretView, "", func(r *http.Request) { r.SetPathValue("accessID", accessID) })
+		if r.statusCode != 303 {
+			t.Errorf("expected 303 status code, got %v", r.statusCode)
+		} else if !strings.HasPrefix(r.headers.Get("Location"), fmt.Sprintf("/secret/%v/", accessID)) {
+			t.Errorf("expected redirect to start with /secret/%v/, got %v", accessID, r.headers.Get("Location"))
+		}
+
+		r = get(t, app.handleAccessSecret, func(hr *http.Request) {
+			hr.SetPathValue("accessID", accessID)
+			hr.SetPathValue("viewingKey", (strings.Split(r.headers.Get("Location"), "/")[3]))
+		})
+		if r.statusCode != 200 {
+			t.Errorf("expected 200 status code, got %v", r.statusCode)
+		}
+
+		var deletedAt sql.NullInt64
+		var deletionReason sql.NullString
+
+		err := app.db.db.
+			QueryRow("SELECT deleted_at, deletion_reason FROM secrets WHERE access_id = ?", accessID).
+			Scan(&deletedAt, &deletionReason)
+
+		if err != nil {
+			t.Errorf("err when querying secret: %v", err)
+		} else if !deletedAt.Valid {
+			t.Errorf("expected secret's deleted_at to have been set")
+		} else if deletionReason.String != deletionReasonMaximumViewCountHit {
+			t.Errorf("expected deletion reason to be maximum_view_count_hit, got %v", deletionReason.String)
+		}
+	})
+
+	t.Run("returns error if secret viewing key has been used already", func(t *testing.T) {
+		accessID, _ := createSecret(t, time.Time{}, "")
+		viewingKey, _ := secureID(8)
+
+		_, err := app.db.db.Exec(
+			`
+				INSERT INTO secret_views (secret_id, viewing_key, viewed_at, created_at)
+				SELECT id, ?, NULL, ?
+				FROM secrets
+				WHERE access_id = ?
+			`,
+			viewingKey,
+			time.Now().UnixMilli(),
+			time.Now().UnixMilli(),
+			accessID,
+		)
+		if err != nil {
+			t.Errorf("failed to create secret_view: %v", err)
+		}
+
+		r := get(t, app.handleAccessSecret, func(hr *http.Request) {
+			hr.SetPathValue("accessID", accessID)
+			hr.SetPathValue("viewingKey", viewingKey)
+		})
+		if r.statusCode != 303 {
+			t.Errorf("expected 303 status code, got %v", r.statusCode)
+		} else if c := r.cookies[0]; c.Name != "flash_err" {
+			t.Errorf("expected flash_err cookie to be present")
+		}
+	})
+}
+
 func post(t *testing.T, endpoint http.HandlerFunc, body string, rc func(r *http.Request)) consumedResponse {
 	recorder := httptest.NewRecorder()
 
@@ -124,10 +205,10 @@ func post(t *testing.T, endpoint http.HandlerFunc, body string, rc func(r *http.
 
 var emptyRequestConfigurer = func(r *http.Request) {}
 
-func get(t *testing.T, endpoint http.HandlerFunc, path string, rc func(r *http.Request)) consumedResponse {
+func get(t *testing.T, endpoint http.HandlerFunc, rc func(r *http.Request)) consumedResponse {
 	recorder := httptest.NewRecorder()
 
-	r, err := http.NewRequest("POST", path, nil)
+	r, err := http.NewRequest("POST", "something", nil)
 	if err != nil {
 		t.Error()
 	}
@@ -172,16 +253,17 @@ func createSecret(t *testing.T, deletedAt time.Time, deletionReason string) (str
 
 	_, err := app.db.db.Exec(
 		`
-			INSERT INTO secrets (access_id, management_id, maximum_views, ttl, deleted_at, deletion_reason, created_at)
-			VALUES (?, ?, ?, ?, ?, ?, ?)
+			INSERT INTO secrets (access_id, management_id, maximum_views, ttl, cipher_text, deleted_at, deletion_reason, created_at)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 		`,
 		accessID,
 		managementID,
 		1,
 		30,
+		"a.b.c",
 		dbDeletedAt,
 		dbDeletionReason,
-		time.Now(),
+		time.Now().UnixMilli(),
 	)
 	if err != nil {
 		t.Errorf("failed creating secret: %v", err)
