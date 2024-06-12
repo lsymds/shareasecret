96527e49660cdebb42076ad07edd46ae42ba8504
Liam Symonds <47109131+lsymds@users.noreply.github.com>
2024-06-12 21:06:29 +0100

    make link copying work on firefox

--- web/js/global.mjs
+++ web/js/global.mjs
@@ -1,16 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 	document.querySelectorAll(".j-button--copy").forEach(function (b) {
 		b.addEventListener("click", async function () {
-			const clipboardPermission = await navigator.permissions.query({
-				name: "clipboard-write",
-			});
-			if (clipboardPermission.state !== "granted") {
-				console.error(
-					".j-button--copy hook: clipboard write permission not granted"
-				);
-				return;
-			}
-
 			const targetAttribute = b.attributes.getNamedItem("data-target");
 			if (!targetAttribute) {
 				console.error(
@@ -27,10 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
 				return;
 			}
 
+			target.removeAttribute("disabled");
 			target.select();
-			target.setSelectionRange(0, 99999);
-
-			navigator.clipboard.writeText(target.value);
+			document.execCommand("copy");
+			target.setAttribute("disabled", true);
 		});
 	});
 });
